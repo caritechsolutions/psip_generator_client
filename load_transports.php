12d65f7a2f53f9e322ddca55de19b446eab10c5d
caritechsolutions <rrawlins@caritech.net>
2025-06-03 03:26:22 -0400

    updates

--- load_transports.php
+++
@@ -1,9 +0,0 @@
-<?php
-$transportsDirectory = 'transports';
-$transports = array_filter(glob("$transportsDirectory/*"), 'is_dir');
-$transports = array_map(function($dir) {
-    return basename($dir);
-}, $transports);
-
-echo json_encode($transports);
-?>
